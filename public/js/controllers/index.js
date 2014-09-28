8f8e5d40ead343bcfc1feba5f4a6a03b32c12c67
Andreas Hultgren <me@andreashultgren.se>
2014-09-28 12:55:51 +0200

    Renamed footer controller to Navigation

--- public/js/controllers/index.js
+++ public/js/controllers/index.js
@@ -5,9 +5,9 @@ module.exports = 'controllers';
 var angular = require('angular');
 var controllers = angular.module(module.exports, []);
 
-controllers.controller('UsersCtrl', require('./UsersCtrl'));
 controllers.controller('HostCtrl', require('./HostCtrl'));
-controllers.controller('ProfileCtrl', require('./ProfileCtrl'));
 controllers.controller('LoginCtrl', require('./LoginCtrl'));
 controllers.controller('LogoutCtrl', require('./LogoutCtrl'));
-controllers.controller('footerMenuCtrl', require('./footerCtrl'));
+controllers.controller('NavigationCtrl', require('./NavigationCtrl'));
+controllers.controller('ProfileCtrl', require('./ProfileCtrl'));
+controllers.controller('UsersCtrl', require('./UsersCtrl'));
