982709aac165a87d2a7cdc3aa2ff780090ab88d2
Andreas Hultgren <me@andreashultgren.se>
2014-11-01 16:53:00 +0100

    Use localStorage instead of sessionStorage

--- public/js/services/SessionStorage.js
+++ public/js/services/SessionStorage.js
@@ -1,16 +1,18 @@
 'use strict';
 
 module.exports = ['$window', function($window) {
+  var store = $window.localStorage;
+
   return {
-    set: function save (key, value) {
-      $window.sessionStorage[key] = JSON.stringify(value);
+    set: function set (key, value) {
+      store.setItem(key, JSON.stringify(value));
       return this;
     },
-    get: function (key, fallback) {
-      return JSON.parse($window.sessionStorage[key] || JSON.stringify(fallback));
+    get: function get (key, fallback) {
+      return JSON.parse(store.getItem(key) || JSON.stringify(fallback));
     },
     remove: function remove (key) {
-      delete $window.sessionStorage[key];
+      store.removeItem(key);
       return this;
     }
   };
